0291fa93d8b266e7ca7884b6435e7cf05f8085a8
Adrien Roches <adrien@roc-it.tech>
2026-03-16 16:19:06 +0100

    Use correct case on Enum + phpdoc on cases

--- src/Enum/RecorderMode.php
+++ src/Enum/RecorderMode.php
@@ -4,8 +4,23 @@ namespace Symfony\HttpClientRecorderBundle\Enum;
 
 enum RecorderMode: string
 {
-    case RECORD = 'record';
-    case PLAYBACK = 'playback';
-    case NEW_EPISODES = 'new_episodes';
-    case PASS_THROUGH = 'passthrough';
+    /**
+     * Records all HTTP requests into the HAR file.
+     */
+    case Record = 'record';
+
+    /**
+     * Replays HTTP requests from the HAR file.
+     */
+    case Playback = 'playback';
+
+    /**
+     * Tries to replay requests from the HAR file, otherwise records them if no match is found.
+     */
+    case NewEpisodes = 'new_episodes';
+
+    /**
+     * Completely ignores the recording system and executes requests normally.
+     */
+    case PassThrough = 'passthrough';
 }
